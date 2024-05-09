bc1c4dbfedd75ab490463c669e1702a1d6d90baa
hajjamn <hajjamn@gmail.com>
2024-05-09 17:21:19 +0200

    ADD: layout mockup

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,32 @@
-<footer>
-  <div class="container">
-    Footer
+<footer class="bg-dark text-white">
+  <div class="container py-3">
+    <div class="row justify-content-center align-items-end">
+      <div class="col-3 me-auto">
+        <p>Copyright by nobody</p>
+      </div>
+      <div class="col-3">
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-transparent text-white border-white">
+            <h4>Contacts</h4>
+          </li>
+          <li class="list-group-item bg-transparent text-white border-white">
+            <p>Facebook</p>
+          </li>
+          <li class="list-group-item bg-transparent text-white border-white">
+            <p>Instagram</p>
+          </li>
+          <li class="list-group-item bg-transparent text-white border-white">
+            <p>
+              Twitter
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-3 ms-auto text-end">
+        <div class="border border-success text-center rounded-pill w-50 d-inline-block">
+          <h4 class="text-success">LOGO</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </footer>
